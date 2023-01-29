d369a3b524378630b2609dd0744c6347024e40a7
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-01-29 20:45:11 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+<template>
+  <div>
+    <h1>Counter</h1>
+    <p>{{ count }}</p>
+    <div>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </div>
+</template>
+<style scoped>
+div {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
